c644f89fb48842b1429e1a6b25bda253cc3297ab
Quangdao Nguyen <me@quangdao.com>
2022-12-05 00:44:03 -0600

    Part 1 Solution

--- day-05/src/functions.js
+++ day-05/src/functions.js
@@ -1,5 +1,76 @@
+export const parseInstructions = (instructions) => {
+  const parts = instructions.split(' ');
+  return {
+    amount: parseInt(parts[1]),
+    from: parseInt(parts[3]),
+    to: parseInt(parts[5]),
+  };
+};
+
+export const parseStacks = (drawing) => {
+  const [stackLabels, ...rows] = drawing
+    .split('\n')
+    .filter((e) => e.trim())
+    .reverse();
+
+  const stacks = {};
+
+  for (const row of rows) {
+    let lastIndexChecked = -1;
+    do {
+      lastIndexChecked = row.indexOf('[', lastIndexChecked + 1);
+      const keyIndex = lastIndexChecked + 1;
+      const stackLabel = stackLabels[keyIndex];
+      const letter = row[keyIndex];
+
+      if (lastIndexChecked < 0) break;
+
+      if (!stacks[stackLabel]) stacks[stackLabel] = [];
+
+      stacks[stackLabel].push(letter);
+    } while (lastIndexChecked > -1);
+  }
+
+  return stacks;
+};
+
+export const executeInstruction = (stacks, instruction) => {
+  const { amount, from, to } = instruction;
+  const source = stacks[from];
+  const destination = stacks[to];
+
+  let movesRemaining = amount;
+
+  while (movesRemaining > 0) {
+    destination.push(source.pop());
+    movesRemaining--;
+  }
+};
+
+export const getTops = (stacks) => {
+  let stringResult = '';
+  for (const key of Object.keys(stacks).sort()) {
+    const stack = stacks[key];
+    stringResult += stack[stack.length - 1];
+  }
+
+  return stringResult;
+};
+
 export const processPart1Solution = (input) => {
-  return 'ABC';
+  const [stacksDrawing, instructionsText] = input
+    .split(/(\r?\n){2}/)
+    .filter((e) => e.trim());
+  const stacks = parseStacks(stacksDrawing);
+  const instructions = instructionsText
+    .split(/\r?\n/)
+    .map((e) => parseInstructions(e));
+
+  for (const instruction of instructions) {
+    executeInstruction(stacks, instruction);
+  }
+
+  return getTops(stacks);
 };
 
 export const processPart2Solution = (input) => {
--- day-05/tests/functions.spec.js
+++ day-05/tests/functions.spec.js
@@ -1,6 +1,109 @@
-import { processPart1Solution } from '../src/functions.js';
+import {
+  executeInstruction,
+  getTops,
+  parseInstructions,
+  parseStacks,
+  processPart1Solution,
+} from '../src/functions.js';
 import fs from 'fs';
 const readFile = fs.promises.readFile;
+
+describe('parseInstructions', () => {
+  it('parses single-digit instructions', () => {
+    const instructions = parseInstructions('move 3 from 1 to 5');
+    expect(instructions).toEqual({
+      amount: 3,
+      from: 1,
+      to: 5,
+    });
+  });
+
+  it('parses multi-digit instructions', () => {
+    const instructions = parseInstructions('move 45 from 12 to 92');
+    expect(instructions).toEqual({
+      amount: 45,
+      from: 12,
+      to: 92,
+    });
+  });
+});
+
+describe('parseStacks', () => {
+  it('parses stacks from sample input', () => {
+    const drawing = `
+    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+    `;
+
+    const result = parseStacks(drawing);
+
+    expect(result).toEqual({
+      1: ['Z', 'N'],
+      2: ['M', 'C', 'D'],
+      3: ['P'],
+    });
+  });
+});
+
+describe('executeInstruction', () => {
+  it('moves single letter instruction', () => {
+    const stacks = {
+      1: ['A', 'B', 'C'],
+      2: ['D', 'E'],
+    };
+
+    executeInstruction(stacks, { amount: 1, from: 1, to: 2 });
+
+    expect(stacks).toEqual({
+      1: ['A', 'B'],
+      2: ['D', 'E', 'C'],
+    });
+  });
+
+  it('inverts multiple letter instruction', () => {
+    const stacks = {
+      1: ['A', 'B', 'C', 'D'],
+      2: ['E', 'F'],
+    };
+
+    executeInstruction(stacks, { amount: 2, from: 1, to: 2 });
+
+    expect(stacks).toEqual({
+      1: ['A', 'B'],
+      2: ['E', 'F', 'D', 'C'],
+    });
+  });
+
+  it('jumps stacks', () => {
+    const stacks = {
+      1: ['A', 'B', 'C', 'D'],
+      2: ['E', 'F'],
+      3: ['G', 'H', 'I'],
+    };
+
+    executeInstruction(stacks, { amount: 2, from: 3, to: 1 });
+
+    expect(stacks).toEqual({
+      1: ['A', 'B', 'C', 'D', 'I', 'H'],
+      2: ['E', 'F'],
+      3: ['G'],
+    });
+  });
+});
+
+describe('getTops', () => {
+  it('gets top crates in single string', () => {
+    const result = getTops({
+      1: ['A', 'B'],
+      2: ['C', 'D', 'E'],
+      3: ['F', 'G'],
+    });
+
+    expect(result).toBe('BEG');
+  });
+});
 
 describe('solutions', () => {
   const INPUT_FILENAME = './inputs/input.sample.txt';
@@ -10,12 +113,12 @@ describe('solutions', () => {
     input = await readFile(INPUT_FILENAME, 'utf-8');
   });
 
-  xit('returns correct solution for part 1', () => {
+  it('returns correct solution for part 1 sample', () => {
     const result = processPart1Solution(input);
     expect(result).toBe('CMZ');
   });
 
-  xit('returns correct solution for part 2', () => {
+  xit('returns correct solution for part 2 sample', () => {
     const result = processPart2Solution(input);
     expect(result).toBe('TBD');
   });
