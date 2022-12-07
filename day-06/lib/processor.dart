314feb87d2aaa06fe906577b0db9cccfb2921ac8
Quangdao Nguyen <me@quangdao.com>
2022-12-06 20:41:40 -0600

    Part 1 Day 6

--- day-06/lib/processor.dart
+++ day-06/lib/processor.dart
@@ -1,5 +1,23 @@
+bool stringHasRepeatingCharacter(String str) {
+  for(int i = 0; i < str.length; i++) {
+    if(str.lastIndexOf(str[i]) > i) return true;
+  }
+  return false;
+}
+
 int processPart1Result(String input) {
-  return 5;
+  int bufferSize = 4;
+  int pointer = bufferSize;
+
+  while(pointer <= input.length) {
+    var check = input.substring(pointer - bufferSize, pointer);
+    if(!stringHasRepeatingCharacter(check)) {
+      return pointer;
+    }
+    pointer++;
+  }
+
+  return -1;
 }
 
 int processPart2Result(String input) {
--- day-06/test/processor_test.dart
+++ day-06/test/processor_test.dart
@@ -3,13 +3,26 @@ import 'package:program/processor.dart';
 import 'package:test/test.dart';
 
 void main() {
+  group('stringHasRepeatingCharacter', () {
+    test('identifies repeated character', () {
+      expect(true, stringHasRepeatingCharacter('aa'));
+      expect(true, stringHasRepeatingCharacter('abca'));
+      expect(true, stringHasRepeatingCharacter('abcdefghh'));
+    });
+
+    test('identifies nonrepeating strings', () {
+      expect(false, stringHasRepeatingCharacter('pz'));
+      expect(false, stringHasRepeatingCharacter('abcd'));
+    });
+  });
+
   group('solutions', () {
     var expectedResults = {
       'mjqjpqmgbljsphdztnvjfqwrcgsmlb': 7,
-      'nppdvjthqldpwncqszvftbrmjlhg': 5,
-      'bvwbjplbgvbhsrlpgdmjqwftvncz': 6,
+      'bvwbjplbgvbhsrlpgdmjqwftvncz': 5,
+      'nppdvjthqldpwncqszvftbrmjlhg': 6,
       'nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg': 10,
-      'zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw': 10
+      'zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw': 11
     };
 
     expectedResults.forEach((input, expected) {
